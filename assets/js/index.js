421e656961bdad94a0272d4af1473b8ecd035334
Arkar Maung <49133212+rkarmaung@users.noreply.github.com>
2019-12-27 03:48:42 +0800

    Changes on burger-Nav at below

--- assets/js/index.js
+++ assets/js/index.js
@@ -58,3 +58,32 @@ window.addEventListener("scroll", function(){
   parallax[0].style.backgroundPositionY = +window.pageYOffset/2+'px';
   parallaxFooter.style.backgroundPositionY = +window.pageYOffset/20+'px';
 });
+
+//burger menu
+var btnLogo = document.querySelector(".btnLogo");
+var nav = document.querySelector(".nav")
+var burgerLogoCounter = 0;
+btnLogo.addEventListener("click", function(){
+  if(burgerLogoCounter==0){
+    nav.style.marginLeft = "0px";
+    btnLogo.style.opacity = "1";
+    btnLogo.style.top = "0";
+    btnLogo.style.left = "250px";
+    btnLogo.style.transform = "scale(1)";
+    btnLogo.style.marginLeft = "-495px";
+    burgerLogoCounter++;
+  } else {
+    nav.style.marginLeft = "-300px";
+    btnLogo.style.opacity = "0.5";
+    btnLogo.style.top = "-80px";
+    btnLogo.style.left = "210px";
+    btnLogo.style.transform = "scale(0.5)";
+    btnLogo.style.marginLeft = "0px";
+    burgerLogoCounter--;
+  }
+});
+
+// var lesserThan1310 = window.matchMedia("(min-width: 1310px)");
+//   if (lesserThan1310.matches){
+//     nav.style.marginLeft = "0";
+//   };
